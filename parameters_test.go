bcbb525adda30809d0f324719c898b445973c5fa
winebarrel <sugawara@winebarrel.jp>
2024-02-06 01:36:56 +0900

    Add port env tests

--- parameters_test.go
+++ parameters_test.go
@@ -53,6 +53,50 @@ func TestParametersGet(t *testing.T) {
 	)
 }
 
+func TestParametersGetWithPortEnv(t *testing.T) {
+	t.Setenv("PARAMETERS_SECRETS_EXTENSION_HTTP_PORT", "7777")
+	assert := assert.New(t)
+	require := require.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(http.MethodGet, "http://localhost:7777/systemsmanager/parameters/get/?name=foo", func(req *http.Request) (*http.Response, error) {
+		return httpmock.NewStringResponse(http.StatusOK, `
+			{
+				"Parameter": {
+						"Name": "MyStringParameter",
+						"Type": "String",
+						"Value": "Veni",
+						"Version": 1,
+						"LastModifiedDate": 1530018761.888,
+						"ARN": "arn:aws:ssm:us-east-2:111222333444:parameter/MyStringParameter",
+						"DataType": "text"
+				}
+			}
+		`), nil
+	})
+
+	p, err := secretlamb.NewParameters()
+	require.NoError(err)
+	value, err := p.Get("foo")
+	require.NoError(err)
+
+	assert.Equal(
+		&secretlamb.ParameterOutput{
+			Parameter: secretlamb.ParameterOutputParameter{
+				Name:             "MyStringParameter",
+				Type:             "String",
+				Value:            "Veni",
+				Version:          1,
+				LastModifiedDate: 1530018761.888,
+				Arn:              "arn:aws:ssm:us-east-2:111222333444:parameter/MyStringParameter",
+				DataType:         "text",
+			},
+		},
+		value,
+	)
+}
+
 func TestParametersGetWithDecryption(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
--- secrets_test.go
+++ secrets_test.go
@@ -49,6 +49,46 @@ func TestSecretsGet(t *testing.T) {
 	)
 }
 
+func TestSecretsGetWithPortEnv(t *testing.T) {
+	t.Setenv("PARAMETERS_SECRETS_EXTENSION_HTTP_PORT", "17777")
+	assert := assert.New(t)
+	require := require.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(http.MethodGet, "http://localhost:17777/secretsmanager/get?secretId=foo", func(req *http.Request) (*http.Response, error) {
+		return httpmock.NewStringResponse(http.StatusOK, `
+			{
+				"ARN": "arn:aws:secretsmanager:us-west-2:123456789012:secret:MyTestSecret-a1b2c3",
+				"Name": "MyTestSecret",
+				"VersionId": "a1b2c3d4-5678-90ab-cdef-EXAMPLE22222",
+				"SecretString": "{\"user\":\"diegor\",\"password\":\"PREVIOUS-EXAMPLE-PASSWORD\"}",
+				"VersionStages": [
+						"AWSPREVIOUS"
+				],
+				"CreatedDate": 1523477145.713
+			}
+		`), nil
+	})
+
+	client, err := secretlamb.NewSecrets()
+	require.NoError(err)
+	value, err := client.Get("foo")
+	require.NoError(err)
+
+	assert.Equal(
+		&secretlamb.SecretOutput{
+			Arn:           "arn:aws:secretsmanager:us-west-2:123456789012:secret:MyTestSecret-a1b2c3",
+			Name:          "MyTestSecret",
+			VersionID:     "a1b2c3d4-5678-90ab-cdef-EXAMPLE22222",
+			SecretString:  "{\"user\":\"diegor\",\"password\":\"PREVIOUS-EXAMPLE-PASSWORD\"}",
+			VersionStages: []string{"AWSPREVIOUS"},
+			CreatedDate:   1523477145.713,
+		},
+		value,
+	)
+}
+
 func TestSecretsGetWithOptions(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
